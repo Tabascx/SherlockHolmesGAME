8452e0861e35aec929e50b0139919d6e9f349e5f
Ismael <ismaelkouira40@gmail.com>
2025-03-02 12:55:26 +0100

    Project Update 4.0

--- SherlockHolmesGAME-master/org/example/juego/Main.java
+++ SherlockHolmesGAME-master/org/example/juego/Main.java
@@ -1,7 +1,9 @@
-package org.example.juego;
+package org.example.Game;
 
 public class Main {
     public static void main(String[] args) {
-
+        Game juego = new Game();
+        System.out.println("¡Bienvenido al juego de Sherlock Holmes!");
+        // Aquí se podrían cargar casos y empezar el juego
     }
 }
